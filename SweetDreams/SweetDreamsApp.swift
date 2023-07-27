8c2cacc99fa9a8fb4805d1edc0662f79c3a93b05
Patrick Bellot <patrickbellot77@gmail.com>
2023-07-27 11:41:57 -0500

    Initial Commit

---
+++ SweetDreams/SweetDreamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetDreamsApp.swift
+//  SweetDreams
+//
+//  Created by Patrick Bellot on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SweetDreamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
